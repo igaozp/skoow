da9ffd0b8f821b2722e5fb92b1b84391ece0b97e
igaozp <igaozp@hotmail.com>
2023-03-10 23:49:44 +0800

    feat: 30 Day JS - Day 1

---
+++ apps/vanilla/30-Days-Of-JavaScript/Day1.js
@@ -0,0 +1,24 @@
+console.log('Hello, World!')
+console.log('Hello', 'World', '!')
+
+console.log(2 + 3)
+console.log(3 - 2)
+console.log(2 * 3)
+console.log(3 / 2)
+console.log(3 % 2)
+console.log(3 ** 2)
+
+let firstName
+console.log(firstName)
+
+let emptyValue = null
+console.log(emptyValue)
+
+console.log(typeof 'ABC')
+console.log(typeof 5)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+
+let name = 'Asabeneh', job = 'teacher', live = 'Finland'
+console.log(name, job, live)
