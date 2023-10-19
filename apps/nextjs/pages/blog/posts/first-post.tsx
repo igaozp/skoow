c9103c7e229b46918a76dd235ffb29da789ff865
igaozp <igaozp@hotmail.com>
2023-10-19 21:27:53 +0800

    feat(nextjs): improve page layout and style

--- apps/nextjs/pages/blog/posts/first-post.tsx
+++ apps/nextjs/pages/blog/posts/first-post.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
 import Head from "next/head";
 import Script from "next/script";
-import Image from "next/image";
 import React from "react";
-import Layout from "../../../components/layout";
 
 export default function FirstPost() {
     return (
-        <Layout>
+        <>
             <Head>
                 <title>First Post</title>
             </Head>
@@ -18,13 +16,11 @@ export default function FirstPost() {
                     console.log(`script loaded correctly, window.FB has been populated`)
                 }
             />
-            <Image src="/images/profile.jpg" alt="Profile" width={144} height={144}>
-            </Image>
             <h1>First Post</h1>
             <h2>
                 <Link href='/blog'>Back to home</Link>
             </h2>
-        </Layout>
+        </>
     );
 }
 
