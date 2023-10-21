b5f5ca8958005d5c582a063f64a617e8aaf0454f
igaozp <igaozp@hotmail.com>
2023-10-21 20:47:43 +0800

    feat(nextjs): SSG example

--- apps/nextjs/pages/blog/index.tsx
+++ apps/nextjs/pages/blog/index.tsx
@@ -1,8 +1,29 @@
 import Head from 'next/head';
 import utilStyles from '../../styles/utils.module.css';
 import Layout, {siteTitle} from "../../components/layout";
+import {getSortedPostsData} from "../../lib/posts";
+import {GetStaticProps} from "next";
 
-export default function Home() {
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
+
+// export const getStaticProps = (async (context) => {
+//     const allPostsData = getSortedPostsData();
+//     return {
+//         props: {
+//             allPostsData
+//         }
+//     }
+// }) satisfies GetStaticProps
+
+// @ts-ignore
+export default function Home({allPostsData}) {
     return (
         <Layout home>
             <Head>
@@ -14,6 +35,23 @@ export default function Home() {
                     (This is a sample website - you’ll be building a site like this on{' '}
                     <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
                 </p>
+            </section>
+
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(
+                        // @ts-ignore
+                        ({id, date, title}) => (
+                            <li className={utilStyles.listItem} key={id}>
+                                {title}
+                                <br/>
+                                {id}
+                                <br/>
+                                {date}
+                            </li>
+                        ))}
+                </ul>
             </section>
         </Layout>
     );
