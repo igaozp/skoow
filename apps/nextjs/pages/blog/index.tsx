d27f813d25b3384653171df603413137de382f5f
igaozp <igaozp@hotmail.com>
2023-11-04 15:31:35 +0800

    feat(nextjs): improve blog example

--- apps/nextjs/pages/blog/index.tsx
+++ apps/nextjs/pages/blog/index.tsx
@@ -36,7 +36,7 @@ export default function Home({allPostsData}) {
                         // @ts-ignore
                         ({id, date, title}) => (
                             <li className={utilStyles.listItem} key={id}>
-                                <Link href={`/posts/${id}`}>{title}</Link>
+                                <Link href={`/blog/posts/${id}`}>{title}</Link>
                                 <br/>
                                 <small className={utilStyles.lightText}>
                                     <Date dateString={date}/>
