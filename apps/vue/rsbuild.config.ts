ae8b5e08096d65278e9de2c95bbde16e79860c05
igaozp <igaozp@hotmail.com>
2025-01-05 15:01:52 +0800

    build(deps): bump vue module version

--- apps/vue/rsbuild.config.ts
+++ apps/vue/rsbuild.config.ts
@@ -1,7 +1,6 @@
 import {defineConfig, loadEnv} from "@rsbuild/core";
 import {pluginVue} from "@rsbuild/plugin-vue";
 import {pluginVueJsx} from "@rsbuild/plugin-vue-jsx";
-import {fileURLToPath, URL} from "node:url";
 import {pluginNodePolyfill} from "@rsbuild/plugin-node-polyfill";
 
 const {publicVars} = loadEnv({prefixes: ["VITE_"]});
@@ -13,9 +12,6 @@ export default defineConfig({
     source: {
         entry: {
             index: "./src/main.ts",
-        },
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
         define: publicVars,
     },
